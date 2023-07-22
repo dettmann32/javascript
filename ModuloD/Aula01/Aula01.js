3b47beea203a64a5492c11ee66cb20aca5b1ac81
Deathman <lucasdettmann8@gmail.com>
2023-07-22 15:51:29 -0300

    commit

---
+++ ModuloD/Aula01/Aula01.js
@@ -0,0 +1,7 @@
+let pais = 'Brasil'
+
+if (pais.toUpperCase() != 'BRASIL'){
+    console.log(`Vivendo em ${pais} voce é estrangeiro`)
+}else{
+   console.log(`vivendo em ${pais} voce é brasileiro`)
+}
