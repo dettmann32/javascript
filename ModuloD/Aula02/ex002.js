104b32c26ea6f5adece0c6c16bc3938142f7a367
Deathman <lucasdettmann8@gmail.com>
2023-07-22 20:26:41 -0300

    commit

---
+++ ModuloD/Aula02/ex002.js
@@ -0,0 +1,14 @@
+
+let horas = 22
+console.log(`Agora são ${horas} horas`)
+    if (horas < 6){
+        console.log('boa madrugada')
+        }else if (horas < 12){
+                console.log('bom dia')
+            }else if (horas <= 18){
+                console.log('boa tarde')
+                }else if (horas > 18){
+                    console.log('boa noite')
+                }
+
+//ver expressão switch
