cec44b5cc4528a423f637d97f3ec5cc000db0609
Deathman <lucasdettmann8@gmail.com>
2023-08-11 18:05:56 -0300

    commit

--- Extra/fetch/main.js
+++ Extra/fetch/main.js
@@ -1,7 +1,13 @@
-let data
-const url = 'https://swapi.dev/api/people/'
-async function name (){
-  const response = await fetch(url)
-  console.log(response)
-    
-}
+fetch('http://localhost:3000/posts')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+   
+  })
+  .catch(error => {
+    console.error('Erro:', error);
+  });
+
+function name(){
+  let a = window.document.getElementById(teste)
+  a.innerText =   'alguma coisa'}
