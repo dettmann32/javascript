2b87e379ef5300a1ba85993aa977c1dd1ebca54a
Deathman <lucasdettmann8@gmail.com>
2023-08-13 16:45:58 -0300

    commit

--- Extra/fetch/main.js
+++ Extra/fetch/main.js
@@ -1,13 +1,22 @@
-fetch('http://localhost:3000/posts')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-   
-  })
-  .catch(error => {
-    console.error('Erro:', error);
-  });
+fetch('http://localhost:3000/posts')  .then(response => {
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json(); // Isso converte a resposta em formato JSON
+})
+.then(data => {
+  // Aqui você pode usar os dados recebidos
+  console.log(data);
+})
+.catch(error => {
+  console.error('There was a problem with the fetch operation:', error);
+});
 
-function name(){
-  let a = window.document.getElementById(teste)
-  a.innerText =   'alguma coisa'}
+
+
+
+
+
+
+
+
